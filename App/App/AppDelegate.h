4b4dff08150f0077fb45358891b6303984e8ecb0
zhang xiuming <zhangxiuming@zhangdeMacBook-Pro.local>
2016-10-15 17:37:55 +0800

    fix

---
+++ App/App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by zhang xiuming on 2016/10/15.
+//  Copyright © 2016年 morenotepad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App/Pods/Target Support Files/MSImagePickerController/MSImagePickerController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MSImagePickerController : NSObject
+@end
+@implementation PodsDummy_MSImagePickerController
+@end
